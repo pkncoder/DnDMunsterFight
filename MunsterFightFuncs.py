5fce3c9cd10b6332efce01d6792f7b141cf1e3e3
pkncoder <128310642+pkncoder@users.noreply.github.com>
2023-05-06 00:15:27 +0000

    Fixed issues, and added the real damage stuff.

--- MunsterFightFuncs.py
+++ MunsterFightFuncs.py
@@ -1,49 +1,83 @@
 import random
 import requests
 
+
 def test(winner, end, MunsBhp, MunsAhp, nameA, nameB):
-  if MunsBhp <= 0:
-    end = True
-    winner = nameA
-  elif MunsAhp <= 0:
-    end = True
-    winner = nameB
-  return winner, end
+    if MunsBhp <= 0:
+        end = True
+        winner = nameA
+    elif MunsAhp <= 0:
+        end = True
+        winner = nameB
+    return winner, end
+
 
 def HitA(dmg, i, MunsAhitDie, rollsA):
-  while i != rollsA:
-      dmgroll = random.randint(1, MunsAhitDie)
-      dmg += dmgroll
-      i += 1
-  return dmg
+    while i != rollsA:
+        dmgroll = random.randint(1, MunsAhitDie)
+        dmg += dmgroll
+        i += 1
+    return dmg
+
 
 def HitB(dmg, i, MunsBhitDie, rollsB):
-  while i != rollsB:
-      dmgroll = random.randint(1, MunsBhitDie)
-      dmg += dmgroll
-      i += 1
-  return dmg
+    while i != rollsB:
+        dmgroll = random.randint(1, MunsBhitDie)
+        dmg += dmgroll
+        i += 1
+    return dmg
+
 
 def inititiveRoll():
-  rollA = random.randint(1, 20)
-  rollB = random.randint(1, 20)
-  if rollA > rollB or rollA == rollB:
-    Roll = 1
-  elif rollA < rollB:
-    Roll = 0
-  return Roll
+    rollA = random.randint(1, 20)
+    rollB = random.randint(1, 20)
+    if rollA > rollB or rollA == rollB:
+        Roll = 1
+    elif rollA < rollB:
+        Roll = 0
+    return Roll
+
 
 def swap(nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB):
-  nameA, nameB = nameB, nameA
-  MunsAhp, MunsBhp = MunsBhp, MunsAhp
-  MunsAAC, MunsBAC = MunsBAC, MunsAAC
-  MunsAHit, MunsBHit = MunsBHit, MunsAHit
-  MunsAhitDie, MunsBhitDie = MunsBhitDie, MunsAhitDie
-  rollsA, rollsB = rollsB, rollsA
-  return nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB
-def response1(where, nameA):
-  response = requests.get(f"https://www.dnd5eapi.co/api/monsters/{nameA}")
-  return response.json()[where]
-def response2(where, nameB):
-  response = requests.get(f"https://www.dnd5eapi.co/api/monsters/{nameB}")
-  return response.json()[where]
+    nameA, nameB = nameB, nameA
+    MunsAhp, MunsBhp = MunsBhp, MunsAhp
+    MunsAAC, MunsBAC = MunsBAC, MunsAAC
+    MunsAHit, MunsBHit = MunsBHit, MunsAHit
+    MunsAhitDie, MunsBhitDie = MunsBhitDie, MunsAhitDie
+    rollsA, rollsB = rollsB, rollsA
+    return nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB
+
+
+def get_monster(name):
+    response = requests.get(f"https://www.dnd5eapi.co/api/monsters/{name}")
+    return response.json()
+
+
+def action_damage(input):
+    # input = "1d8+3"
+
+    # Test type of input
+    if "+" not in input and "d" not in input: 
+        return input, 1, 0
+
+    # Get the damage modifer 
+    if "+" in input:
+        input_split_1 = input.split("+")
+        modifier = int(input_split_1[1])
+        input = input_split_1[0]
+        
+    else: 
+        modifier = 0
+
+    # Break apart damage die 
+    if "d" in input:
+        damage_die = input_split_1[0]
+        damage_die_split = damage_die.split("d")
+        damage_die_count = int(damage_die_split[0])
+        damage_die_type = int(damage_die_split[1])
+
+    else:
+        damage_die_count = 0
+        damage_die_type = 0 
+
+    return damage_die_count, damage_die_type, modifier
--- main.py
+++ main.py
@@ -1,171 +1,168 @@
 #! /usr/bin/python3
 
-import requests
 import random
 from MunsterFightFuncs import *
-from time import sleep
 
 print("This is a simulator that has two monsters fight.")
 print("This is random and will have different outputs every time!")
 
-play = input("Would you like to customize the stats of the creature, or would you like the real stats? Real or Custom: ")
+play = input(
+    "Would you like to customize the stats of the creature, or would you like the real stats? Real or Custom: ")
 play = play.lower()
 
 if play == 'custom':
-  nameA = input("What is the first monster's name?\n")
-  nameB = input("What is the second monster's name?\n")
+    nameA = input("What is the first monster's name?\n")
+    nameB = input("What is the second monster's name?\n")
 
-  MunsAhp = int(input(f"What is {nameA}'s Hp?\n"))
-  MunsAAC = int(input(f"What is {nameA}'s AC?\n"))
-  MunsAhitDie = int(input(f"What is the hit die of {nameA}?\n"))
-  rollsA = int(input("How many times will the hit die be rolled?\n"))
+    MunsAhp = int(input(f"What is {nameA}'s Hp?\n"))
+    MunsAAC = int(input(f"What is {nameA}'s AC?\n"))
+    MunsAhitDie = int(input(f"What is the hit die of {nameA}?\n"))
+    rollsA = int(input("How many times will the hit die be rolled?\n"))
 
-  MunsBhp = int(input(f"What is {nameB}'s Hp?\n"))
-  MunsBAC = int(input(f"What is {nameB}'s AC?\n"))
-  MunsBhitDie = int(input(f"What is the hit die of {nameB}?\n"))
-  rollsB = int(input("How many times will the hit die be rolled?\n"))
+    MunsBhp = int(input(f"What is {nameB}'s Hp?\n"))
+    MunsBAC = int(input(f"What is {nameB}'s AC?\n"))
+    MunsBhitDie = int(input(f"What is the hit die of {nameB}?\n"))
+    rollsB = int(input("How many times will the hit die be rolled?\n"))
 
-  end = False
-  InGameTime = 0
+    end = False
+    InGameTime = 0
 
-  winner = ()
-  MunsAHit = ()
-  MunsBHit = ()
+    winner = ()
+    MunsAHit = ()
+    MunsBHit = ()
 
-  Roll = inititiveRoll()
+    Roll = inititiveRoll()
 
-  if Roll == 0:
-    nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB = swap(nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB)
+    if Roll == 0:
+        nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB = swap(
+            nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB)
 
-  while end == False:
-    print(f"{nameA} attacts!")
-    MunsAHit = random.randint(1, 20)
-    
-    if MunsAHit >= MunsBAC:
-      print(f"{nameA} hits with a {MunsAHit}!")
+    while end == False:
+        print(f"{nameA} attacts!")
+        MunsAHit = random.randint(1, 20)
 
-      dmg = 0
-      i = 0
+        if MunsAHit >= MunsBAC:
+            print(f"{nameA} hits with a {MunsAHit}!")
 
-      dmg = HitA(dmg, i, MunsAhitDie, rollsA)
-      
-      print(f'Damage is {dmg}!')
-      MunsBhp -= dmg
-      
-    else:
-      print(f"{nameA} did not hit with a {MunsAHit}. Sad.")
+            dmg = 0
+            i = 0
 
-    print(f"{nameB} = {MunsBhp}")
-    print(f"{nameA} = {MunsAhp}")
-    input("Press the enter key to continue...")
+            dmg = HitA(dmg, i, MunsAhitDie, rollsA)
 
-    print(f"{nameB} attacts!")
-    MunsBHit = random.randint(1, 20)
-    
-    if MunsBHit >= MunsAAC:
-      print(f"{nameB} hit with a {MunsBHit}.")
+            print(f'Damage is {dmg}!')
+            MunsBhp -= dmg
 
-      dmg = 0
-      i = 0
+        else:
+            print(f"{nameA} did not hit with a {MunsAHit}. Sad.")
 
-      dmg = HitB(dmg, i, MunsBhitDie, rollsB)
+        print(f"{nameB} = {MunsBhp}")
+        print(f"{nameA} = {MunsAhp}")
+        input("Press the enter key to continue...")
 
-      print(f"Dmg is {dmg}")
-      MunsAhp -= dmg
-      
-    else:
-      print(f"{nameB} did not hit with a {MunsBHit}.")
-    
-    InGameTime += 6
-    winner, end = test(winner, end, MunsBhp, MunsAhp, nameA, nameB)
+        print(f"{nameB} attacts!")
+        MunsBHit = random.randint(1, 20)
 
-    print(f"{nameB} = {MunsBhp}")
-    print(f"{nameA} = {MunsAhp}")
-    input("Press the enter key to continue...")
-    
-  print("There is a winner!")
-  print(f"The winner is {winner}!")
-  print(f"That took {InGameTime} seconds in game.")
-  print(f"In other words that took {InGameTime / 60} minute(s).")
+        if MunsBHit >= MunsAAC:
+            print(f"{nameB} hit with a {MunsBHit}.")
+
+            dmg = 0
+            i = 0
+
+            dmg = HitB(dmg, i, MunsBhitDie, rollsB)
+
+            print(f"Dmg is {dmg}")
+            MunsAhp -= dmg
+
+        else:
+            print(f"{nameB} did not hit with a {MunsBHit}.")
+
+        InGameTime += 6
+        winner, end = test(winner, end, MunsBhp, MunsAhp, nameA, nameB)
+
+        print(f"{nameB} = {MunsBhp}")
+        print(f"{nameA} = {MunsAhp}")
+        input("Press the enter key to continue...")
+
+    print("There is a winner!")
+    print(f"The winner is {winner}!")
+    print(f"That took {InGameTime} seconds in game.")
+    print(f"In other words that took {InGameTime / 60} minute(s).")
 elif play == 'real':
-  nameA = input("What is the first monster's name?\n")
-  nameB = input("What is the second monster's name?\n")
-  
-  where = 'hit_points'
-  MunsAhp = response1(where, nameA)
-  where = 'armor_class'
-  MunsAAC = response1(where, nameA)
-  
-  MunsAhitDie = int(input(f"What is the damage die for {nameA}(max amount of damage that can be done, ex. 8, or 6): "))
-  rollsA = int(input(f"What is the amount of times the damage die will be rolled for {nameA}: "))
+    nameA = input("What is the first monster's name?\n")
+    nameB = input("What is the second monster's name?\n")
 
-  where = 'hit_points'
-  MunsBhp = response2(where, nameB)
-  where = 'armor_class'
-  MunsBAC = response2(where, nameB)
+    monsterA = get_monster(name=nameA)
+    monsterB = get_monster(name=nameB)
 
-  MunsBhitDie = int(input(f"What is the damage die for {nameB}(max amount of damage that can be done, ex. 8, or 6): "))
-  rollsB = int(input(f"What is the amount of times the damage die will be rolled for {nameB}: "))
+    MunsAhp = monsterA["hit_points"]
+    MunsAAC = monsterA["armor_class"][0]["value"]
 
-  end = False
-  InGameTime = 0
+    rollsA, MunsAhitDie, MunsAMod = action_damage(monsterA["actions"][0]["damage"][0]["damage_dice"])
+    rollsB, MunsBhitDie, MunsBMod = action_damage(monsterB["actions"][0]["damage"][0]["damage_dice"])
 
-  winner = ()
-  MunsAHit = ()
-  MunsBHit = ()
+    MunsBhp = monsterB["hit_points"]
+    MunsBAC = monsterB["armor_class"][0]["value"]
 
-  Roll = inititiveRoll()
+    end = False
+    InGameTime = 0
 
-  if Roll == 0:
-    nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB = swap(nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB)
+    winner = ()
+    MunsAHit = ()
+    MunsBHit = ()
 
-  while end == False:
-    print(f"{nameA} attacts!")
-    MunsAHit = random.randint(1, 20)
-    
-    if MunsAHit >= MunsBAC:
-      print(f"{nameA} hits with a {MunsAHit}!")
+    Roll = inititiveRoll()
 
-      dmg = 0
-      i = 0
+    if Roll == 0:
+        nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB = swap(
+           nameA, nameB, MunsAhp, MunsBhp, MunsAAC, MunsBAC, MunsAHit, MunsBHit, MunsAhitDie, MunsBhitDie, rollsA, rollsB)
 
-      dmg = HitA(dmg, i, MunsAhitDie, rollsA)
-      
-      print(f'Damage is {dmg}!')
-      MunsBhp -= dmg
-      
-    else:
-      print(f"{nameA} did not hit with a {MunsAHit}. Sad.")
+    while end == False:
+        print(f"{nameA} attacts!")
+        MunsAHit = random.randint(1, 20)
 
-    print(f"{nameB} = {MunsBhp}")
-    print(f"{nameA} = {MunsAhp}")
-    input("Press the enter key to continue...")
+        if MunsAHit >= MunsBAC:
+            print(f"{nameA} hits with a {MunsAHit}!")
 
-    print(f"{nameB} attacts!")
-    MunsBHit = random.randint(1, 20)
-    
-    if MunsBHit >= MunsAAC:
-      print(f"{nameB} hit with a {MunsBHit}.")
+            dmg = 0
+            i = 0
 
-      dmg = 0
-      i = 0
+            dmg = HitA(dmg, i, MunsAhitDie, rollsA)
 
-      dmg = HitB(dmg, i, MunsBhitDie, rollsB)
+            print(f'Damage is {dmg}!')
+            MunsBhp -= dmg
 
-      print(f"Dmg is {dmg}")
-      MunsAhp -= dmg
-      
-    else:
-      print(f"{nameB} did not hit with a {MunsBHit}.")
-    
-    InGameTime += 6
-    winner, end = test(winner, end, MunsBhp, MunsAhp, nameA, nameB)
+        else:
+            print(f"{nameA} did not hit with a {MunsAHit}. Sad.")
 
-    print(f"{nameB} = {MunsBhp}")
-    print(f"{nameA} = {MunsAhp}")
-    input("Press the enter key to continue...")
-    
-  print("There is a winner!")
-  print(f"The winner is {winner}!")
-  print(f"That took {InGameTime} seconds in game.")
-  print(f"In other words that took {InGameTime / 60} minute(s).")
+        print(f"{nameB} = {MunsBhp}")
+        print(f"{nameA} = {MunsAhp}")
+        input("Press the enter key to continue...")
+
+        print(f"{nameB} attacts!")
+        MunsBHit = random.randint(1, 20)
+
+        if MunsBHit >= MunsAAC:
+            print(f"{nameB} hit with a {MunsBHit}.")
+
+            dmg = 0
+            i = 0
+
+            dmg = HitB(dmg, i, MunsBhitDie, rollsB)
+
+            print(f"Dmg is {dmg}")
+            MunsAhp -= dmg
+
+        else:
+            print(f"{nameB} did not hit with a {MunsBHit}.")
+
+        InGameTime += 6
+        winner, end = test(winner, end, MunsBhp, MunsAhp, nameA, nameB)
+
+        print(f"{nameB} = {MunsBhp}")
+        print(f"{nameA} = {MunsAhp}")
+        input("Press the enter key to continue...")
+
+    print("There is a winner!")
+    print(f"The winner is {winner}!")
+    print(f"That took {InGameTime} seconds in game.")
+    print(f"In other words that took {InGameTime / 60} minute(s).")
